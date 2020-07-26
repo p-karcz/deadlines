7241b6b17a4cab68579ccb2a402d4e2f52f229db
Piotr Karcz <piotrkarcz2000@gmail.com>
2020-07-26 19:56:45 +0200

    initial commit

---
+++ app/src/main/java/com/example/deadlines/DeadlinesMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.deadlines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeadlinesMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.deadlines_main_activity)
+    }
+}
